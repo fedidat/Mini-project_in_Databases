f7b7502fc02651a0dff5d6ff0015def2d4aeca1e
fedidat <fedidat@gmail.com>
2013-04-04 15:16:18 +0300

    Changed model, generated data

---
+++ Model/Oracle/loadInsurance.bat
@@ -0,0 +1,18 @@
+ 
+sqlldr userid=SCOTT/TIGER control=Customer.ctl log=Customer.log
+
+ 
+sqlldr userid=SCOTT/TIGER control=Policies.ctl log=Policies.log
+
+ 
+sqlldr userid=SCOTT/TIGER control=Customers_Policies.ctl log=Customers_Policies.log
+
+ 
+sqlldr userid=SCOTT/TIGER control=Life.ctl log=Life.log
+
+ 
+sqlldr userid=SCOTT/TIGER control=Property.ctl log=Property.log
+
+ 
+sqlldr userid=SCOTT/TIGER control=Claims.ctl log=Claims.log
+
